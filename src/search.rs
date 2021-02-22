485f6d5739d01dfd72745a19efce69cbd7e6145a
Styvane Soukossi <mstyvane@gmail.com>
2021-02-22 16:49:15 +0100

    Recursively find maximum subarray

--- src/search.rs
+++ src/search.rs
@@ -1,6 +1,7 @@
 //! This module contains utilities functions for searching
 //! an item in a container.
 
+use num_traits::{Num, NumOps};
 use std::cmp::{self, Ordering};
 
 /// Linear search for a key in a container.
@@ -96,6 +97,86 @@ where
     }
 }
 
+// Find maximum crossing subarray.
+#[allow(dead_code)]
+fn find_maximum_cross_subarray<T>(
+    arr: &[T],
+    lower: usize,
+    mid: usize,
+    upper: usize,
+) -> (usize, usize, T)
+where
+    T: cmp::Ord + Copy + Num + NumOps,
+{
+    let mut max_left = mid - 1;
+    let mut left_sum = arr[mid - 1];
+    let mut sum = arr[mid - 1];
+
+    for i in (lower..mid).rev() {
+        if i < mid - 1 {
+            sum = sum + arr[i];
+            if left_sum < sum {
+                left_sum = sum;
+                max_left = i;
+            }
+        }
+    }
+
+    let mut max_right = mid;
+    let mut sum = arr[mid];
+    let mut right_sum = arr[mid];
+    for i in mid..upper {
+        if i > mid {
+            sum = sum + arr[i];
+            if right_sum < sum {
+                right_sum = sum;
+                max_right = i;
+            }
+        }
+    }
+
+    (max_left, max_right, left_sum + right_sum)
+}
+
+/// Find a maximum subarray of container using recursion
+///
+/// # Examples
+///
+/// ```
+/// use alda::search;
+///
+/// let a = &[-99, -1, 2, 9, -11,  -3, 4, 89, -2];
+/// assert_eq!(search::recursive_find_maximum_subarray(a, 0, a.len()), (6, 7, 93))
+/// ```
+///
+pub fn recursive_find_maximum_subarray<T>(
+    container: &[T],
+    lower: usize,
+    upper: usize,
+) -> (usize, usize, T)
+where
+    T: cmp::Ord + Copy + Num + NumOps,
+{
+    assert!(!container.is_empty());
+
+    if lower == upper - 1 {
+        return (lower, lower, container[lower]);
+    }
+    let mid = (lower + upper) / 2;
+    let (left_lower, left_upper, left_sum) = recursive_find_maximum_subarray(container, lower, mid);
+    let (right_lower, right_upper, right_sum) =
+        recursive_find_maximum_subarray(container, mid, upper);
+    let (cross_lower, cross_upper, cross_sum) =
+        find_maximum_cross_subarray(container, lower, mid, upper);
+    if left_sum >= right_sum && left_sum > cross_sum {
+        (left_lower, left_upper, left_sum)
+    } else if right_sum >= left_sum && right_sum >= cross_sum {
+        (right_lower, right_upper, right_sum)
+    } else {
+        (cross_lower, cross_upper, cross_sum)
+    }
+}
+
 #[cfg(test)]
 use quickcheck_macros::quickcheck;
 
@@ -137,5 +218,20 @@ mod tests {
         let key = &xs[ix];
         let found = recursive_binsearch(&xs, key);
         found == Some(ix)
+    }
+
+    #[test]
+    fn test_maximum_cross_array() {
+        let a = &[9, -1, 2, 9, -11, -3, 4, 9, -2];
+        assert_eq!(
+            find_maximum_cross_subarray(a, 0, a.len() / 2, a.len()),
+            (0, 7, 18)
+        )
+    }
+
+    #[test]
+    fn test_recursive_find_maximum_subarray() {
+        let a = &[9, -1, 2, 9, -11, -3, 4, 9, -2];
+        assert_eq!(recursive_find_maximum_subarray(a, 0, 6), (0, 3, 19))
     }
 }
