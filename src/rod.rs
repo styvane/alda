099f96d671e3fb428c9dca7666f020b6a01d21a5
Styvane Soukossi <mstyvane@gmail.com>
2021-03-30 11:00:33 +0200

    rod: optimize rod cutting maximum using top-down memoization

--- src/rod.rs
+++ src/rod.rs
@@ -3,6 +3,7 @@
 //! This module contains a various implementation of rod cutting algorithms.
 
 use std::cmp;
+use std::collections::HashMap;
 
 /// The type `Rod` contains the data for computing the maximum revenue
 /// for cutting a rod and selling pieces.
@@ -18,22 +19,52 @@ impl<'a> Rod<'a> {
 
     /// Recursively find the maximum revenue for cutting a rod and selling
     /// it pieces.
-    pub fn recursive_maximum(&self, n: usize) -> usize {
-        fn compute_maximum(prices: &Vec<usize>, n: usize) -> usize {
-            let mut p = 0;
+    pub fn recursive_maximum(&self, size: usize) -> usize {
+        fn compute_maximum(prices: &Vec<usize>, size: usize) -> usize {
+            let mut max = 0;
 
             for (i, value) in prices
                 .iter()
                 .enumerate()
                 .skip(1)
-                .take_while(|(i, _)| i <= &n)
+                .take_while(|(i, _)| i <= &size)
             {
-                p = cmp::max(p, value + compute_maximum(prices, n - i))
+                max = cmp::max(max, value + compute_maximum(prices, size - i))
             }
-            p
+            max
         }
 
-        compute_maximum(self.prices, n)
+        compute_maximum(self.prices, size)
+    }
+
+    /// Find the maximum revenue for cutting a rod and selling
+    /// it pieces using a top-down approach with memoization.
+    pub fn maximum_with_memoization(&self, size: usize) -> usize {
+        let mut cache = HashMap::<usize, usize>::new();
+
+        fn memoize_max(
+            prices: &Vec<usize>,
+            size: usize,
+            cache: &mut HashMap<usize, usize>,
+        ) -> usize {
+            if cache.contains_key(&size) {
+                return cache[&size];
+            }
+
+            let mut max = 0;
+            for (i, value) in prices
+                .iter()
+                .enumerate()
+                .skip(1)
+                .take_while(|(i, _)| i <= &size)
+            {
+                max = cmp::max(max, value + memoize_max(prices, size - i, cache));
+            }
+            cache.entry(size).or_insert(max);
+            max
+        }
+
+        memoize_max(self.prices, size, &mut cache)
     }
 }
 
@@ -64,5 +95,22 @@ mod tests {
         let rod = Rod::new(&v);
 
         assert_eq!(rod.recursive_maximum(1), 0);
+    }
+
+    #[test]
+    fn test_memoized_maximum() {
+        let v = vec![0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
+        let rod = Rod::new(&v);
+
+        assert_eq!(rod.maximum_with_memoization(1), 1);
+        assert_eq!(rod.maximum_with_memoization(2), 5);
+        assert_eq!(rod.maximum_with_memoization(3), 8);
+        assert_eq!(rod.maximum_with_memoization(4), 10);
+        assert_eq!(rod.maximum_with_memoization(5), 13);
+        assert_eq!(rod.maximum_with_memoization(6), 17);
+        assert_eq!(rod.maximum_with_memoization(7), 18);
+        assert_eq!(rod.maximum_with_memoization(8), 22);
+        assert_eq!(rod.maximum_with_memoization(9), 25);
+        assert_eq!(rod.maximum_with_memoization(10), 30);
     }
 }
